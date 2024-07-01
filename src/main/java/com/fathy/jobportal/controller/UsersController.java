42c8a4ef99f70e217d185e80dc8f6b032b52f843
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-01 17:57:45 +0300

    Bug Fixed : duplicate email - UsersController.java

--- src/main/java/com/fathy/jobportal/controller/UsersController.java
+++ src/main/java/com/fathy/jobportal/controller/UsersController.java
@@ -6,12 +6,14 @@ import com.fathy.jobportal.services.UsersService;
 import com.fathy.jobportal.services.UsersTypeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
@@ -35,7 +37,17 @@ public class UsersController {
     }
 
     @PostMapping("/register/new")
-    public String userRegistration(@Valid Users users){
+    public String userRegistration(@Valid Users users, Model model){
+
+        Optional<Users> optionalUsers = usersService.getUserByEmail(users.getEmail());
+        if (optionalUsers.isPresent()) {
+            model.addAttribute("error", "Email Already registered," +
+                    " try to login or register with other email.");
+            List<UsersType> usersTypes = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersTypes);
+            model.addAttribute("user", new Users());
+            return "register";
+        }
 //        System.out.println("User:: "+users);
         usersService.addNew(users);
         return "dashboard";
