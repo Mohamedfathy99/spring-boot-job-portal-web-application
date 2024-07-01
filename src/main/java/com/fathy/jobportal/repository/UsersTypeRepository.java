742eb48f4c37581a556303cd0aa6d78e49ca2304
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-01 09:21:34 +0300

    Create UsersTypeRepository.java

---
+++ src/main/java/com/fathy/jobportal/repository/UsersTypeRepository.java
@@ -0,0 +1,11 @@
+package com.fathy.jobportal.repository;
+
+import com.fathy.jobportal.entity.UsersType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+
+
+}
