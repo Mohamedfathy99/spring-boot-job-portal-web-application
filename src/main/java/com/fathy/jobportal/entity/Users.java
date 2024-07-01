6640f2be09d186800f8d25003ff8b8cbe03e6208
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-01 17:31:21 +0300

    Update Users.java

--- src/main/java/com/fathy/jobportal/entity/Users.java
+++ src/main/java/com/fathy/jobportal/entity/Users.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Users {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
     @Column(unique = true)
