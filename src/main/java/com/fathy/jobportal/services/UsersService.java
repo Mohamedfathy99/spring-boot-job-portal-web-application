86d6fb5244227e5354acdef8cdf326471c345a96
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-01 10:41:52 +0300

    Update UsersService.java

--- src/main/java/com/fathy/jobportal/services/UsersService.java
+++ src/main/java/com/fathy/jobportal/services/UsersService.java
@@ -1,8 +1,25 @@
 package com.fathy.jobportal.services;
 
+import com.fathy.jobportal.entity.Users;
+import com.fathy.jobportal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class UsersService {
-    
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users addNew(Users users){
+        users.setActive(true);
+        users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        return usersRepository.save(users);
+    }
 }
