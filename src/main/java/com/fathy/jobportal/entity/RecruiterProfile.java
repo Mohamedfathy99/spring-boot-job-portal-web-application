5b682df66933d93b8514b0f39004abc0ebae9381
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-02 15:50:55 +0300

    Update RecruiterProfile.java

--- src/main/java/com/fathy/jobportal/entity/RecruiterProfile.java
+++ src/main/java/com/fathy/jobportal/entity/RecruiterProfile.java
@@ -40,6 +40,10 @@ public class RecruiterProfile {
         this.profilePhoto = profilePhoto;
     }
 
+    public RecruiterProfile(Users users) {
+        this.userId = users;
+    }
+
     public int getUserAccountId() {
         return userAccountId;
     }
