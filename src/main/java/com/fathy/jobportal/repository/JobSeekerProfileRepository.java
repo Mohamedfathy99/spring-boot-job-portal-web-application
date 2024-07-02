7928586c79e0878586612ce3f38f364603607f10
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-02 15:50:43 +0300

    Create JobSeekerProfileRepository.java

---
+++ src/main/java/com/fathy/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,9 @@
+package com.fathy.jobportal.repository;
+
+import com.fathy.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
