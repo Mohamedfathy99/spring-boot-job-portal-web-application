24e360f8982577504bc49c6b82792d21d25d3d57
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-03 05:14:38 +0300

    Custom Authentication Success Handler - Create CustomUserDetails.java

---
+++ src/main/java/com/fathy/jobportal/util/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.fathy.jobportal.util;
+
+
+import com.fathy.jobportal.entity.Users;
+import com.fathy.jobportal.entity.UsersType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Users users;
+
+    @Autowired
+    public CustomUserDetails(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UsersType userType = users.getUserTypeId();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(userType.getUserTypeName()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
