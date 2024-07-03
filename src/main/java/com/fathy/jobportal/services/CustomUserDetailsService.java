2e868b9091c56a5f13d08d3d6c65e7193fee8da4
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-03 05:14:04 +0300

    Custom Authentication Success Handler - Create CustomUserDetailsService.java

---
+++ src/main/java/com/fathy/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.fathy.jobportal.services;
+
+import com.fathy.jobportal.entity.Users;
+import com.fathy.jobportal.repository.UsersRepository;
+import com.fathy.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(users);
+    }
+}
