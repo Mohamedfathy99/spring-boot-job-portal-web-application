2b77df6f4792535d610ff523e7975cf9cbaa2dbf
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-21 11:45:45 +0300

    Update JobPostActivityRepository.java - Display and Retrieve Jobs

--- src/main/java/com/fathy/jobportal/repository/JobPostActivityRepository.java
+++ src/main/java/com/fathy/jobportal/repository/JobPostActivityRepository.java
@@ -2,9 +2,13 @@ package com.fathy.jobportal.repository;
 
 import com.fathy.jobportal.entity.JobPostActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
 
+    List<IRecruiterJobs> getRecruiterJobs(@Param("recruiter") int recruiter);
 }
