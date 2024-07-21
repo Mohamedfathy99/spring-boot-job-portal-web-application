bc6c5561c46b5ce8c133ba1e05615e60e187af81
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-21 12:09:27 +0300

    Create IRecruiterJobs.java - Display and Retrieve Jobs

---
+++ src/main/java/com/fathy/jobportal/entity/IRecruiterJobs.java
@@ -0,0 +1,22 @@
+package com.fathy.jobportal.entity;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+
+    int getJob_post_id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+
+    String getState();
+
+    String getCountry();
+
+    int getCompanyId();
+
+    String getName();
+}
