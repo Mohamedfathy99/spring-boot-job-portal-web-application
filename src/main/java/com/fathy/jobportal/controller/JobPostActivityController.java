3751874b73fd474a2b1bcdc5897b21b0ec8a8808
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-21 11:44:46 +0300

    Update JobPostActivityController.java - Display and Retrieve Jobs

--- src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
@@ -7,6 +7,7 @@ import com.fathy.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,6 +37,9 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUserName = authentication.getName();
             model.addAttribute("username", currentUserName);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+
+            }
         }
         model.addAttribute("user", currentUserProfile);
         System.out.println("Dashboard");
