581b142b5070448acb3271d8d8b556b7e1a9b708
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-21 10:17:05 +0300

    Update JobPostActivityController.java - Integrate HTML form for posting a new job

--- src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
@@ -1,5 +1,8 @@
 package com.fathy.jobportal.controller;
 
+import com.fathy.jobportal.entity.JobPostActivity;
+import com.fathy.jobportal.entity.Users;
+import com.fathy.jobportal.services.JobPostActivityService;
 import com.fathy.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -8,15 +11,22 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class JobPostActivityController {
 
     private final UsersService usersService;
 
+    private final JobPostActivityService jobPostActivityService;
+
     @Autowired
-    public JobPostActivityController(UsersService usersService) {
+    public JobPostActivityController(UsersService usersService,
+                                     JobPostActivityService jobPostActivityService) {
         this.usersService = usersService;
+        this.jobPostActivityService = jobPostActivityService;
     }
 
     @GetMapping("/dashboard/")
@@ -30,5 +40,25 @@ public class JobPostActivityController {
         model.addAttribute("user", currentUserProfile);
         System.out.println("Dashboard");
         return "dashboard";
+    }
+
+    @GetMapping("/dashboard/add")
+    public String addJobs(Model model){
+        model.addAttribute("jobPostActivity", new JobPostActivity());
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "add-jobs";
+    }
+
+
+    @PostMapping("/dashboard/addNew")
+    public String addNew(JobPostActivity jobPostActivity, Model model){
+        Users user = usersService.getCurrentUser();
+        if (user != null) {
+            jobPostActivity.setPostedById(user);
+        }
+        jobPostActivity.setPostedDate(new Date());
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
+        return "redirect:/dashboard/";
     }
 }
