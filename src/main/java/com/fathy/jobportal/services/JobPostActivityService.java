5e8746853f439a95960b9ee7d2484e5985b37f8f
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-23 01:56:11 +0300

    Edit a Job - View job details

--- src/main/java/com/fathy/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/fathy/jobportal/services/JobPostActivityService.java
@@ -37,4 +37,10 @@ public class JobPostActivityService {
         }
         return recruiterJobsDtoList;
     }
+
+    public JobPostActivity getOne(int id) {
+
+        return jobPostActivityRepository.findById(id).
+                orElseThrow(()-> new RuntimeException("Job not found"));
+    }
 }
