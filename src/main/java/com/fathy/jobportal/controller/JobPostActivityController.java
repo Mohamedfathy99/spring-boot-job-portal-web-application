1bcff323342535755606280d86d0220ceb2b26e5
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-23 02:25:34 +0300

    Update and solve issue in edit job

--- src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/fathy/jobportal/controller/JobPostActivityController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
@@ -71,4 +72,14 @@ public class JobPostActivityController {
         JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
         return "redirect:/dashboard/";
     }
+
+    @PostMapping("dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model){
+
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "add-jobs";
+    }
+
 }
