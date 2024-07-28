93475a5c37ddbfa5c7ccf75c77859e8fbe481c76
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-28 08:23:38 +0300

    Create Entity: JobSeekerApply.java

---
+++ src/main/java/com/fathy/jobportal/entity/JobSeekerApply.java
@@ -0,0 +1,95 @@
+package com.fathy.jobportal.entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"userId","jobPostActivity"})
+})
+public class JobSeekerApply implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "jobPostActivity", referencedColumnName = "jobPostId")
+    private JobPostActivity jobPostActivity;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date applyDate;
+
+    private String coverLetter;
+
+    public JobSeekerApply() {
+    }
+
+    public JobSeekerApply(Integer id, JobSeekerProfile userId,
+                          JobPostActivity jobPostActivity,
+                          Date applyDate, String coverLetter) {
+        this.id = id;
+        this.userId = userId;
+        this.jobPostActivity = jobPostActivity;
+        this.applyDate = applyDate;
+        this.coverLetter = coverLetter;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+
+    public JobPostActivity getJobPostActivity() {
+        return jobPostActivity;
+    }
+
+    public void setJobPostActivity(JobPostActivity jobPostActivity) {
+        this.jobPostActivity = jobPostActivity;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getCoverLetter() {
+        return coverLetter;
+    }
+
+    public void setCoverLetter(String coverLetter) {
+        this.coverLetter = coverLetter;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerApply{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", jobPostActivity=" + jobPostActivity +
+                ", applyDate=" + applyDate +
+                ", coverLetter='" + coverLetter + '\'' +
+                '}';
+    }
+}
