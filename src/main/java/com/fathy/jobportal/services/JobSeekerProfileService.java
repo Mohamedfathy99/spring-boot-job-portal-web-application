2a9001f1b11ab2e62ea1331d66cbd8f555b30d4f
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2024-07-29 04:59:06 +0300

    Update Controller for Searching and Applying for Job - Part 4

--- src/main/java/com/fathy/jobportal/services/JobSeekerProfileService.java
+++ src/main/java/com/fathy/jobportal/services/JobSeekerProfileService.java
@@ -1,8 +1,15 @@
 package com.fathy.jobportal.services;
 
 import com.fathy.jobportal.entity.JobSeekerProfile;
+import com.fathy.jobportal.entity.RecruiterProfile;
+import com.fathy.jobportal.entity.Users;
 import com.fathy.jobportal.repository.JobSeekerProfileRepository;
+import com.fathy.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +18,12 @@ import java.util.Optional;
 public class JobSeekerProfileService {
 
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final UsersRepository usersRepository;
 
     @Autowired
-    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository, UsersRepository usersRepository) {
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<JobSeekerProfile> getOne(Integer id){
@@ -23,5 +32,18 @@ public class JobSeekerProfileService {
 
     public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
         return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+
+    public JobSeekerProfile getCurrentSeekerProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).
+                    orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<JobSeekerProfile> jobSeekerProfile = getOne(users.getUserId());
+            return jobSeekerProfile.orElse(null);
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/fathy/jobportal/services/RecruiterProfileService.java
+++ src/main/java/com/fathy/jobportal/services/RecruiterProfileService.java
@@ -1,8 +1,14 @@
 package com.fathy.jobportal.services;
 
 import com.fathy.jobportal.entity.RecruiterProfile;
+import com.fathy.jobportal.entity.Users;
 import com.fathy.jobportal.repository.RecruiterProfileRepository;
+import com.fathy.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +17,12 @@ import java.util.Optional;
 public class RecruiterProfileService {
 
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersRepository usersRepository;
 
     @Autowired
-    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository, UsersRepository usersRepository) {
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<RecruiterProfile> getOneRecruiter(Integer id){
@@ -23,5 +31,18 @@ public class RecruiterProfileService {
 
     public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
         return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).
+                    orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<RecruiterProfile> recruiterProfile = getOneRecruiter(users.getUserId());
+            return recruiterProfile.orElse(null);
+        } else {
+            return null;
+        }
     }
 }
